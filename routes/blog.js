9cec2017647fdd5b2e155d314acacd1769639c71
Ukabix <54454097+Ukabix@users.noreply.github.com>
2021-12-16 11:37:22 +0100

    create post and display posts list

--- routes/blog.js
+++ routes/blog.js
@@ -1,23 +1,81 @@
-const express = require('express');
+const express = require("express");
+
+// import mongodb
+const mongodb = require("mongodb");
 
 // import db handler file
 const db = require("../data/database");
 
+// call objectId from mongoDb
+const ObjectId = mongodb.ObjectId;
+
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  res.redirect('/posts');
+router.get("/", function (req, res) {
+  res.redirect("/posts");
 });
 
-router.get('/posts', function(req, res) {
-  res.render('posts-list');
+// handle displaying posts list
+router.get("/posts", async function (req, res) {
+  // use projection on find - author.name is nested, so in quotes
+  const posts = await db
+    .getDb()
+    .collection("posts")
+    .find(
+      {},
+      { title: 1, summary: 1, "author.name": 1 }
+    )
+    .toArray();
+  res.render("posts-list", { posts: posts });
 });
 
-router.get('/new-post', async function(req, res) {
-  // get db with collection method - returns a promise
-  const authors = await db.getDb().collection("authors").find().toArray();
-  // pass authors as a key
-  res.render('create-post', { authors: authors });
+router.get(
+  "/new-post",
+  async function (req, res) {
+    // get db with collection method - returns a promise
+    const authors = await db
+      .getDb()
+      .collection("authors")
+      .find()
+      .toArray();
+    // pass authors as a key
+    res.render("create-post", {
+      authors: authors,
+    });
+  }
+);
+
+// add route to incoming blog posts
+router.post("/posts", async function (req, res) {
+  /// fetch author name
+  // create authorId via ObjectId
+  const authorId = new ObjectId(req.body.author);
+  // find relevant author via authorId
+  const author = await db
+    .getDb()
+    .collection("authors")
+    .findOne({ _id: authorId });
+
+  // post a new post
+  const newPost = {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.email,
+    },
+  };
+
+  // insert data to db
+  const result = await db
+    .getDb()
+    .collection("posts")
+    .insertOne(newPost);
+  console.log(result);
+  res.redirect("/posts");
 });
 
 module.exports = router;
